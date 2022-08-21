f1f5f8e30357d922cb3bb8a070c756b96478dcbc
momohana <ttnarita@gmail.com>
2022-08-21 13:36:09 +0900

    change tfstate

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -2,4 +2,4 @@
 
 cd ${CODEBUILD_SRC_DIR}/example
 terraform init -input=false -no-color
-terraform apply -input=false -no-color -auto-approve | tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify-apply.yml apply
+terraform apply -input=false -no-color -auto-approve
